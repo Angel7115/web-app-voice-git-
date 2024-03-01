7651f46b18fae295a7fc5fcbf279257649211f13
Angel Jafet Alvarez Melo <ask200_@hotmail.com>
2024-03-01 01:24:10 -0600

    Version inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('voice-btn').addEventListener('click', function() {
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'es-ES';
+
+    recognition.onresult = function(event) {
+      const transcript = event.results[0][0].transcript;
+      document.getElementById('result-container').textContent = transcript;
+    };
+
+    recognition.onerror = function(event) {
+      console.error('Error de reconocimiento de voz: ' + event.error);
+    };
+
+    recognition.start();
+  });
+});
